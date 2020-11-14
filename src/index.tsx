eb5ddc21ba59ac2cf78a28f6c4e582f2641005ac
Nate Moore <nate@natemoo.re>
2020-11-14 12:30:01 -0600

    feat: go all-in on preact

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,4 @@
 #!/usr/bin/env node
-import type React from "react";
-declare module "react" {
-  interface HTMLAttributes<T>
-    extends React.AriaAttributes,
-      React.DOMAttributes<T> {
-    class?: string;
-  }
-}
-
 import { build } from "./scripts/build";
 async function run() {
   const [command, ...args] = process.argv.slice(2);
