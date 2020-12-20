65f50170fa7117a0e5cc68057a46ba438c538f96
Nate Moore <nate@natemoo.re>
2020-12-20 15:03:34 -0600

    chore: add hello-world example

---
+++ examples/hello-world/src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { FunctionalComponent } from 'preact';
+import { definePage } from 'microsite/page';
+import { Head, seo } from 'microsite/head';
+
+import Counter from '@/components/Counter';
+
+interface IndexProps {}
+
+const Index: FunctionalComponent<IndexProps> = () => {
+  return (
+    <>
+      <Head>
+        <seo.title>Microsite</seo.title>
+
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="alternate icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Microsite!</h1>
+
+        <Counter />
+      </main>
+    </>
+  );
+};
+
+export default definePage(Index);
