5f054d15ff6aaaaad9c6096fdfa1cf1693fda923
Nate Moore <nate@natemoo.re>
2020-12-19 21:40:59 -0600

    chore: temp redirect site back to repo

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -2,40 +2,26 @@ import { FunctionalComponent } from "preact";
 import { definePage } from "microsite/page";
 import { Head, seo } from "microsite/head";
 
-import { resolve, join } from 'path';
-import { promises as fsp } from 'fs';
-import remark from "remark";
-import html from "remark-html";
+import css from './index.module.css';
+import Logo from '@/components/Logo';
 
-const Index: FunctionalComponent<{ content?: string }> = ({ content }) => {
+const Index: FunctionalComponent = () => {
   return (
     <>
       <Head>
         <seo.title>Microsite</seo.title>
+        <seo.description>
+          Microsite is a next-generation static site generator built on top of Snowpack,
+          including features like Automatic Partial Hydration.
+        </seo.description>
       </Head>
 
-      <main>
-        <article dangerouslySetInnerHTML={{ __html: content }}></article>
+      <main class={css.main}>
+        <Logo />
+        <h1>This site is under construction.</h1>
       </main>
     </>
   );
 };
 
-async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
-}
-
-export default definePage(Index, {
-  async getStaticProps() {
-    const readme = resolve(join('..', `README.md`));
-    const content = await fsp.readFile(readme, "utf8");
-    const html = await markdownToHtml(content);
-
-    return {
-      props: {
-        content: html
-      }
-    }
-  }
-});
+export default definePage(Index);
