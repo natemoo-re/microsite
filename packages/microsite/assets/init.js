cb6fd6440134ed422d49b740af560ce6cea76441
Nate Moore <nate@natemoo.re>
2020-12-22 21:00:39 -0600

    feat: use persistent fragment hydration

--- packages/microsite/assets/init.js
+++ packages/microsite/assets/init.js
@@ -1,77 +1,182 @@
-import { h, hydrate as mount } from 'preact';
+import { h, hydrate as mount } from "preact";
 
 const createObserver = (hydrate) => {
-  if (!('IntersectionObserver' in window)) return null;
+  if (!("IntersectionObserver" in window)) return null;
 
   const io = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      const isIntersecting = entry.isIntersecting || entry.intersectionRatio > 0;
+      const isIntersecting =
+        entry.isIntersecting || entry.intersectionRatio > 0;
       if (!isIntersecting) return;
       hydrate();
       io.disconnect();
-    })
+    });
   });
 
   return io;
-}
+};
 
-function attach($cmp, { key, name, source }) {
-  const method = $cmp.dataset.method;
+function attach(fragment, data, { key, name, source }) {
+  const { p: { children = null, ...props } = {}, m: method = "idle" } = data;
 
   const hydrate = async () => {
-    if ($cmp.dataset.hydrate === '') return;
-    if (window.__MICROSITE_DEBUG) console.log(`[Hydrate] <${key} /> hydrated via "${method}"`);
-    const { [name]: Component } = await import(source); 
-    const props = $cmp.dataset.props ? JSON.parse(atob($cmp.dataset.props)) : {};
-    mount(h(Component, props, null), $cmp);
-    delete $cmp.dataset.props;
-    delete $cmp.dataset.method;
-    $cmp.dataset.hydrate = '';
-  }
+    if (window.__MICROSITE_DEBUG)
+      console.log(`[Hydrate] <${key} /> hydrated via "${method}"`);
+    const { [name]: Component } = await import(source);
+    mount(h(Component, props, children), fragment);
+  };
 
   switch (method) {
-    case 'idle': {
-      if (!('requestIdleCallback' in window) || !('requestAnimationFrame' in window)) return hydrate();
+    case "idle": {
+      if (
+        !("requestIdleCallback" in window) ||
+        !("requestAnimationFrame" in window)
+      )
+        return hydrate();
 
-      requestIdleCallback(() => {
-        requestAnimationFrame(hydrate);
-      }, { timeout: 2000 });
+      requestIdleCallback(
+        () => {
+          requestAnimationFrame(hydrate);
+        },
+        { timeout: 2000 }
+      );
       break;
     }
-    case 'interaction': {
-      const events = ['focus', 'click', 'touchstart', 'pointerenter'];
+    case "interaction": {
+      const events = ["focus", "click", "touchstart", "pointerenter"];
       function handleEvent(event) {
         hydrate().then(() => {
-          if (event.type === 'focus') event.target.focus();
+          if (event.type === "focus") event.target.focus();
           for (const e of events) {
             event.target.removeEventListener(e, handleEvent);
           }
-        })
+        });
       }
 
+      const children = fragment.childNodes.filter(
+        (node) => node.nodeType === node.ELEMENT_NODE
+      );
       for (const e of events) {
-        $cmp.addEventListener(e, handleEvent, { once: true, passive: true, capture: true });
+        for (const child of children) {
+          child.addEventListener(e, handleEvent, {
+            once: true,
+            passive: true,
+            capture: true,
+          });
+        }
       }
       break;
     }
-    case 'visible': {
-      if (!('IntersectionObserver' in window)) return hydrate();
+    case "visible": {
+      if (!("IntersectionObserver" in window)) return hydrate();
 
       const observer = createObserver(hydrate);
-      Array.from($cmp.children).forEach(child => observer.observe(child));
+      const child = fragment.childNodes.find(
+        (node) => node.nodeType === node.ELEMENT_NODE
+      );
+      observer.observe(child);
       break;
     }
   }
+}
+
+function createPersistentFragment(parentNode, childNodes) {
+  const last = childNodes && childNodes[childNodes.length - 1].nextSibling;
+  function insert(child, before) {
+    parentNode.insertBefore(child, before || last);
+  }
+  return {
+    parentNode,
+    childNodes,
+    appendChild: insert,
+    insertBefore: insert,
+    removeChild(child) {
+      parentNode.removeChild(child);
+    },
+  };
+}
+
+const ATTR_REGEX = /(:?\w+)=(\w+|[{[].*?[}\]])/g;
+function parseHydrateBoundary(node) {
+  if (!node.textContent) return {};
+  const text = node.textContent.slice("?h ".length, -1);
+
+  let props = {};
+  let result = ATTR_REGEX.exec(text);
+  while (result) {
+    let [, attr, val] = result;
+    if (attr === "p") {
+      props[attr] = JSON.parse(val);
+    } else {
+      props[attr] = val;
+    }
+    result = ATTR_REGEX.exec(text);
+  }
+  return props;
+}
+
+function findHydrationPoints() {
+  const nodeIterator = document.createNodeIterator(
+    document.documentElement,
+    NodeFilter.SHOW_COMMENT,
+    {
+      acceptNode(node) {
+        if (node.textContent && node.textContent.startsWith("?h c"))
+          return NodeFilter.FILTER_ACCEPT;
+        return NodeFilter.FILTER_REJECT;
+      },
+    }
+  );
+
+  const toHydrate = [];
+
+  while (nodeIterator.nextNode()) {
+    const start = nodeIterator.referenceNode;
+    const data = parseHydrateBoundary(start);
+    const childNodes = [];
+
+    let end = start.nextSibling;
+    while (end) {
+      if (
+        end.nodeType === end.COMMENT_NODE &&
+        end.textContent &&
+        end.textContent.startsWith("?h p")
+      ) {
+        Object.assign(data, parseHydrateBoundary(end));
+        break;
+      }
+      childNodes.push(end);
+      end = end.nextSibling;
+    }
+
+    toHydrate.push([
+      createPersistentFragment(start.parentNode, childNodes),
+      data,
+      [start, end],
+    ]);
+  }
+  return toHydrate;
 }
 
 export default (manifest) => {
-  const $cmps = Array.from(document.querySelectorAll('[data-hydrate]'));
-  
-  for (const $cmp of $cmps) {
-    const key = $cmp.dataset.hydrate;
-    const [name, source] = manifest[key];
-    if (name && source) {
-      attach($cmp, { key, name, source });
+  const init = () => {
+    const $cmps = findHydrationPoints();
+
+    for (const [fragment, data, markers] of $cmps) {
+      const { c: Component } = data;
+      const [name, source] = manifest[Component];
+      if (name && source) {
+        attach(fragment, data, { name, source });
+      }
+
+      requestIdleCallback(() => {
+        markers.forEach((marker) => marker.remove());
+      });
     }
+  };
+  if ("requestIdleCallback" in window) {
+    requestIdleCallback(init, { timeout: 1000 });
+  } else {
+    init();
   }
-}
+};
--- packages/microsite/src/hydrate.tsx
+++ packages/microsite/src/hydrate.tsx
@@ -2,7 +2,6 @@ import { h, FunctionComponent, createContext } from "preact";
 import { useContext } from "preact/hooks";
 
 const isServer = typeof window === "undefined";
-const btoa = (str: string) => Buffer.from(str, "utf-8").toString("base64");
 export const HydrateContext = createContext<string | false>(false);
 
 export interface HydrationProps {
@@ -14,7 +13,7 @@ export function withHydrate<T extends FunctionComponent<any>>(
   hydrationProps: HydrationProps = {}
 ): T {
   const name = Component.displayName || Component.name;
-  const { method = "idle" } = hydrationProps;
+  const { method } = hydrationProps;
 
   const Wrapped: FunctionComponent<any> = (props, ref) => {
     const hydrateParent = useContext(HydrateContext);
@@ -28,12 +27,14 @@ export function withHydrate<T extends FunctionComponent<any>>(
         `withHydrate() is unable to serialize complex \`children\`. Please inline these children into <${name} />.`
       );
 
-    const data = isServer ? JSON.stringify(props) : '';
+    const p = isServer ? `p=${JSON.stringify(props)}` : '';
+    const m = isServer && method ? `m=${method}` : '';
+    const Marker = 'hydrate-marker' as any;
     return (
       <HydrateContext.Provider value={name}>
-        {isServer && (<noscript dangerouslySetInnerHTML={{ __html: `<!--$_h:${name}-->` }} />)}
+        {isServer && (<Marker dangerouslySetInnerHTML={{ __html: `?h c=${name} ?` }} />)}
         <Component {...{ ...props, ref }} />
-        {isServer && (<noscript dangerouslySetInnerHTML={{ __html: `<!--$h:${method}:${data}-->` }} />)}
+        {isServer && (<Marker dangerouslySetInnerHTML={{ __html: `?h ${[p, m].filter(v => v).join(' ')} ?` }} />)}
       </HydrateContext.Provider>
     );
   };
