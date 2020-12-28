35a746dfec0e7b8ac2d30e1ada55cf23112495cb
Nate Moore <nate@natemoo.re>
2020-12-28 09:02:56 -0600

    chore(examples): fix copy-examples script

--- copy-examples.js
+++ copy-examples.js
@@ -1,9 +1,11 @@
 import fs from 'fs-extra';
 const { readdir, copy } = fs;
+import path from 'path';
+const { resolve } = path;
 
 async function run() {
     const examples = (await readdir('./examples')).filter(v => v.indexOf('.') === -1);
-    await Promise.all(examples.map(example => copy(`./examples/${example}/dist`, './examples/dist', { recursive: true })));
+    await Promise.all(examples.map(example => copy(resolve(process.cwd(), `./examples/${example}/dist`), resolve(process.cwd(), './examples/dist'), { recursive: true })));
 }
 
 run();
