0551a51dfbd887c7e7133e84f0d0f3680f7399b0
Nate Moore <nate@natemoo.re>
2020-12-28 08:49:28 -0600

    chore(examples): fix generate examples

--- examples/root/scripts/generate-examples.js
+++ examples/root/scripts/generate-examples.js
@@ -1,10 +1,11 @@
 import { promises as fsp } from 'fs';
+import { resolve } from 'path'
 
 async function run() {
     const ents = await fsp.readdir('../', { withFileTypes: true });
     let results = ents.map((ent) => ent.isDirectory() ? ent.name : null).filter(name => name && name !== 'root')
 
-    await fsp.writeFile('./examples/root/src/examples.json', JSON.stringify(results));
+    await fsp.writeFile(resolve(process.cwd(), './examples/root/src/examples.json'), JSON.stringify(results));
 }
 
 run();
