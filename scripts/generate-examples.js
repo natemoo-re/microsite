567a573b138ce8efe6aed20399c232c098840340
Nate Moore <nate@natemoo.re>
2021-01-29 04:42:18 -0600

    fix: properly resolve example script in CI

--- scripts/generate-examples.js
+++ scripts/generate-examples.js
@@ -1,11 +1,15 @@
 import { promises as fsp } from 'fs';
-import { resolve } from 'path'
+import { join } from 'path'
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 async function run() {
+    const base = join(require.resolve('lerna').split('/node_modules')[0], 'examples');
     const ents = await fsp.readdir('./examples', { withFileTypes: true });
     let results = ents.map((ent) => ent.isDirectory() ? ent.name : null).filter(name => name && !['dist', '.microsite', 'root'].includes(name))
 
-    await fsp.writeFile(resolve(process.cwd(), './examples/examples.json'), JSON.stringify(results));
+    await fsp.writeFile(join(base, 'examples.json'), JSON.stringify(results));
 }
 
 run();
