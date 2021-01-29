08193b91362a84d1f34e7a9ff6d38fdfba45eee5
Nate Moore <nate@natemoo.re>
2021-01-29 04:25:53 -0600

    chore(examples): update example build

--- scripts/copy-examples.js
+++ scripts/copy-examples.js
@@ -5,10 +5,11 @@ import { resolve } from 'path'
 const { copy } = fse;
 
 async function run() {
-    const ents = await fsp.readdir('../', { withFileTypes: true });
-    let results = ents.map((ent) => ent.isDirectory() ? ent.name : null).filter(name => name && name !== 'root')
+    const ents = await fsp.readdir('./examples', { withFileTypes: true });
+    let results = ents.map((ent) => ent.isDirectory() ? ent.name : null).filter(name => name && !['dist', '.microsite', 'root'].includes(name))
 
-    await Promise.all(results.map((example) => copy(resolve(process.cwd(), `./examples/${example}/dist`), resolve(process.cwd(), `./examples/dist`), { recursive: true })));
+    await copy(resolve(process.cwd(), `./examples/root/dist`), resolve(process.cwd(), `./examples/dist`), { recursive: true });
+    await Promise.all(results.map((example) => copy(resolve(process.cwd(), `./examples/${example}/dist/${example}`), resolve(process.cwd(), `./examples/dist/${example}`), { recursive: true })));
 }
 
 run();
--- scripts/generate-examples.js
+++ scripts/generate-examples.js
@@ -2,10 +2,10 @@ import { promises as fsp } from 'fs';
 import { resolve } from 'path'
 
 async function run() {
-    const ents = await fsp.readdir('../', { withFileTypes: true });
-    let results = ents.map((ent) => ent.isDirectory() ? ent.name : null).filter(name => name && name !== 'root')
+    const ents = await fsp.readdir('./examples', { withFileTypes: true });
+    let results = ents.map((ent) => ent.isDirectory() ? ent.name : null).filter(name => name && !['dist', '.microsite', 'root'].includes(name))
 
-    await fsp.writeFile(resolve(process.cwd(), './examples.json'), JSON.stringify(results));
+    await fsp.writeFile(resolve(process.cwd(), './examples/examples.json'), JSON.stringify(results));
 }
 
 run();
