199b37d3021d9697816e817bce3e0b40d4306f8e
Nate Moore <nate@natemoo.re>
2021-03-01 16:46:21 -0600

    chore(bench): normalize benchmarks

--- benchmark/microsite-simple/src/pages/index.tsx
+++ benchmark/microsite-simple/src/pages/index.tsx
@@ -1,27 +1,14 @@
 import { FunctionalComponent } from "preact";
 import { definePage } from "microsite/page";
-import { Head, seo } from "microsite/head";
-import styles from "./Home.module.css";
+import styles from "./index.module.css";
 
 interface IndexProps {}
 
 const Index: FunctionalComponent<IndexProps> = () => {
   return (
-    <>
-      <Head>
-        <seo.title>Microsite</seo.title>
-
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="alternate icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to{" "}
-          <a href="https://github.com/natemoo-re/microsite">Microsite!</a>
-        </h1>
-      </main>
-    </>
+    <main className={styles.main}>
+      <h1>Hello world!</h1>
+    </main>
   );
 };
 
--- benchmark/next-simple/pages/index.tsx
+++ benchmark/next-simple/pages/index.tsx
@@ -1,19 +1,9 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "./index.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
+    <main className={styles.main}>
+      <h1>Hello world!</h1>
+    </main>
   );
 }
